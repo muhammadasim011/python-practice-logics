8c80729117b0374231004ab89c7a9c94e9f40ad7
Mohammad Asim <asimmangrio840@gmail.com>
2025-08-04 18:29:05 +0500

    Add Snake Water Gun game with clean input and win logic

---
+++ snake_water_gun.py.py
@@ -0,0 +1,38 @@
+import random
+"""
+snake 1
+water -1
+gun 0
+"""
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w": -1, "g":0}
+reverseDict = {1:"Snake", -1: "Water", 0: "gun"}
+
+if youstr not in youDict:
+    print("Invalid input! Choose 's', 'w', or 'g'.")
+    exit()
+
+you = youDict[youstr]
+print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
+
+
+
+if(computer == you):
+    print("Its a draw!")
+else:
+    if(computer == -1 and you == 1):
+        print("You win!")
+    elif(computer == -1 and you == 0):
+        print("You lose!")  
+    elif(computer == 1 and you == -1):
+        print("You lose!")
+    elif(computer == 1 and you == 0):
+        print("You win!")
+    elif(computer == 0 and you == 1):
+        print("You lose!")
+    elif(computer == 0 and you == -1):
+        print("You win!") 
+    else:
+        print("Something went wrong!")
